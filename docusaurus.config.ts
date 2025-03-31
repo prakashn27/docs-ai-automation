f671056dfebd382b7902081dd6da823ff3790acc
Prakash Natarajan <nprakashece@gmail.com>
2025-03-31 13:42:09 +0000

    revert: gtm tag

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
           customCss: './src/css/custom.css',
         },
         googleTagManager: {
-          containerId: 'G-7LGY6ESECX',
+          containerId: 'GTM-WX4NQXQL',
         },
       } satisfies Preset.Options,
     ],
